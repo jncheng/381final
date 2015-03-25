7953dbca4baec521d2a0889393dd36d5be42273c
jncheng <jncheng@sfu.ca>
2015-03-25 11:55:18 -0700

    update

--- combo/js/app.js
+++ combo/js/app.js
@@ -177,6 +177,24 @@ var JQueryMobilePage = React.createClass({
   }
 });
 
+var x = document.getElementById("altitude");
+var a = document.getElementById("speed");
+var h = document.getElementById('direction');
+getLocation();
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.watchPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";}
+    }
+    
+function showPosition(position) {
+    x.innerHTML="Altitude: " + position.coords.altitude.toFixed(1) + "meters";
+    a.innerHTML="Speed: " + position.coords.speed.toFixed(1) + "m/s";  
+    direction.innerHTML = "Direction: " + position.coords.heading;  
+}
+
 /** Application page home component. */
 var PageHomeContent = React.createClass({
   displayName: 'PageHomeContent',
@@ -206,8 +224,9 @@ var PageHomeContent = React.createClass({
         React.DOM.p(null, '')
 
         
+        )
       )
-      )
+   
     );
   }
 });
